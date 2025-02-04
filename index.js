56ebeaf7aed51784da8c5f7f6efe1ef160fd25eb
caleb <cakpomughe@gmail.com>
2025-02-04 14:57:10 +0100

    math type from numbers api

--- index.js
+++ index.js
@@ -5,26 +5,6 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-function isPrime(n) {
-    if (n < 2) return false;
-    for (let i = 2; i * i <= n; i++) {
-        if (n % i === 0) return false;
-    }
-    return true;
-}
-
-function isPerfect(n) {
-    if (n < 1) return false; // Perfect numbers are positive only
-    let sum = 1;
-    for (let i = 2; i * i <= n; i++) {
-        if (n % i === 0) {
-            sum += i;
-            if (i !== n / i) sum += n / i;
-        }
-    }
-    return sum === n && n !== 1;
-}
-
 function isArmstrong(n) {
     let sum = 0, temp = Math.abs(n), digits = temp.toString().length;
     while (temp > 0) {
@@ -40,7 +20,7 @@ const fetchFunFact = async (num) => {
         setTimeout(() => source.cancel(), 300); // Cancel if request takes >300ms
 
         return await Promise.race([
-            axios.get(`http://numbersapi.com/${num}`, { cancelToken: source.token }).then(res => res.data),
+            axios.get(`http://numbersapi.com/${num}/math`, { cancelToken: source.token }).then(res => res.data),
             new Promise((resolve) => setTimeout(() => resolve("No fun fact available"), 300))
         ]);
     } catch {
@@ -56,23 +36,16 @@ app.get("/api/classify-number", async (req, res) => {
         return res.status(400).json({ error: true, number });
     }
 
-    const properties = [];
-    if (num % 2 === 0) properties.push("even");
-    else properties.push("odd");
+    const isEven = num % 2 === 0;
+    const isArmstrongNum = isArmstrong(num);
 
-    if (isPrime(num)) properties.push("prime");
-    if (num >= 0 && isPerfect(num)) properties.push("perfect"); // Avoid negatives
-    if (isArmstrong(num)) properties.push("armstrong"); // Works for both positive & negative
-
-    properties.sort(); // Ensure properties are sorted
+    let properties = isArmstrongNum ? (isEven ? ["armstrong", "even"] : ["armstrong", "odd"]) : (isEven ? ["even"] : ["odd"]);
 
     const digitSum = Math.abs(num).toString().split("").reduce((sum, d) => sum + parseInt(d), 0);
     const funFact = await fetchFunFact(num);
 
     res.json({
         number: num,
-        is_prime: isPrime(num),
-        is_perfect: isPerfect(num),
         properties,
         digit_sum: digitSum,
         fun_fact: funFact
