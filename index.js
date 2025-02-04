bba7a1a2222073dad23e4c9414cbf8c4a4c05686
caleb <cakpomughe@gmail.com>
2025-02-04 14:12:11 +0100

    numbers api

--- index.js
+++ index.js
@@ -73,5 +73,5 @@ app.get("/api/classify-number", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
